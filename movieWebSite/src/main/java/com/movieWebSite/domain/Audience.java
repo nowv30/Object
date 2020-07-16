ccb49abc0ab7ea8c3fa320769ceb01cf54fabb71
kowvic <52282781+kowvic@users.noreply.github.com>
2020-07-16 11:36:30 +0900

    설계 개선하기(Audience)
    Audience에 자율성 추가하기

--- movieWebSite/src/main/java/com/movieWebSite/domain/Audience.java
+++ movieWebSite/src/main/java/com/movieWebSite/domain/Audience.java
@@ -8,4 +8,17 @@ public class Audience {
 	public Bag getBag() {
 		return bag;
 	}
+	
+	//3-1. TicketSeller의 SellTo 메소드의 로직을 Audience가 직접 수행한다.
+	//   Audience 스스로 Bag의 내용물을 확인하고 다른 곳에서 알지 못하게 한다.
+	public Long buy(Ticket ticket) {
+		if(bag.hasInvitation()) {
+			bag.setTicket(ticket);
+			return 0L;
+		} else {
+			bag.setTicket(ticket);
+			bag.minusAmount(ticket.getFee());
+			return ticket.getFee();
+		}		
+	}
 }
